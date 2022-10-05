b2e305b4d3ba32267f27c5fd503691a70c7ae737
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-05 08:03:56 -0300

    Added pagination to show numbers

--- components/PrimesList.tsx
+++ components/PrimesList.tsx
@@ -1,12 +1,54 @@
+import { useState } from "react";
+
 interface PrimesListProps {
   primes: number[];
 }
 export function PrimesList({ primes }: PrimesListProps) {
+  const [pagination, setPagination] = useState({ perPage: 10, page: 0 });
   return (
-    <ul className="flex gap-2 flex-wrap">
-      {primes.map((primeNumber) => (
-        <li key={primeNumber}>{primeNumber};</li>
-      ))}
-    </ul>
+    <>
+      <ul className="flex gap-2 flex-wrap mt-4 text-white">
+        {primes
+          .slice(
+            pagination.page * pagination.perPage,
+            pagination.perPage * (pagination.page + 1)
+          )
+          .map((primeNumber) => (
+            <li key={primeNumber}>{primeNumber};</li>
+          ))}
+      </ul>
+      <section className="mt-2">
+        <div className="flex justify-between">
+          <span>Per page: {pagination.perPage}</span>
+          <span>Page: {pagination.page + 1}</span>
+        </div>
+        <div className="mt-2 flex justify-between">
+          <button
+            onClick={() =>
+              setPagination((previousState) => {
+                if (previousState.page === 0) return previousState;
+                return { ...previousState, page: previousState.page - 1 };
+              })
+            }
+          >
+            Back
+          </button>
+          <button
+            onClick={() =>
+              setPagination((previousState) => {
+                if (
+                  previousState.page >=
+                  primes.length - previousState.perPage * (pagination.page + 1)
+                )
+                  return previousState;
+                return { ...previousState, page: previousState.page + 1 };
+              })
+            }
+          >
+            Next
+          </button>
+        </div>
+      </section>
+    </>
   );
 }
