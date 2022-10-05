e598d4dee183fd6baae8785cca5f852eaa6e4189
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-05 11:19:51 -0300

    Added write array function

---
+++ utils/writeArrayInDb.ts
@@ -0,0 +1,8 @@
+import { writeJsonFileSync } from "write-json-file";
+import { getDb } from "./getDb";
+export function writeArrayInDb(dbName: string, data: [any]) {
+  const { db, dbPath } = getDb(dbName);
+  const entireData = [...db[dbName], ...data];
+  writeJsonFileSync(dbPath, { [dbName]: entireData });
+  return entireData;
+}
