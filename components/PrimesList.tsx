993384e40b9f72276b5e5ad375cf76de0f54c5c2
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-14 18:49:00 -0300

    Added reverse primes order feature

--- components/PrimesList.tsx
+++ components/PrimesList.tsx
@@ -6,17 +6,35 @@ interface PrimesListProps {
 }
 export function PrimesList({ primes }: PrimesListProps) {
   const [pagination, setPagination] = useState({ perPage: 10, page: 0 });
+  const [reversed, setReversed] = useState(false);
+  let initialPosition = pagination.page * pagination.perPage;
+  let endPosition = pagination.perPage * (pagination.page + 1);
+  if (reversed) {
+    initialPosition = primes.length - endPosition;
+    endPosition = primes.length - pagination.perPage * pagination.page;
+
+    if (initialPosition < 0) initialPosition = 0;
+    if (endPosition < 0) endPosition = primes.length;
+  }
+  const primeList = primes
+    .slice(initialPosition, endPosition)
+    .map((primeNumber) => <li key={primeNumber}>{primeNumber};</li>);
   return (
     <>
-      <ul className="flex gap-2 flex-wrap mt-4 text-white">
-        {primes
-          .slice(
-            pagination.page * pagination.perPage,
-            pagination.perPage * (pagination.page + 1)
-          )
-          .map((primeNumber) => (
-            <li key={primeNumber}>{primeNumber};</li>
-          ))}
+      <section className="flex justify-end mt-4 sm:mt-6">
+        <div>
+          <label>
+            <span className="mr-4">Reversed</span>
+            <input
+              type="checkbox"
+              checked={reversed}
+              onChange={() => setReversed(!reversed)}
+            />
+          </label>
+        </div>
+      </section>
+      <ul className="flex gap-2 flex-wrap mt-4">
+        {reversed ? primeList.reverse() : primeList}
       </ul>
       <Pagination
         pagination={pagination}
