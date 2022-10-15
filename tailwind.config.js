3bcd9217218609802e82baf5122cd52e729dd79c
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-14 22:38:53 -0300

    Added bgs and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: {
+          darker: "#492912",
+          "darker-hover": "#73401C",
+          default: "#D49D76",
+          light: "#F0D0C0",
+          secundary: "#2293E2",
+        },
+      },
+      colors: {
+        primary: "#5E3711",
+        secundary: "#2293E2",
+      },
+    },
   },
   plugins: [],
 };
